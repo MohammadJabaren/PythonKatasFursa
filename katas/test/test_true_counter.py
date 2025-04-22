3d1d08023b4b02f4bc2bfdc3b6da5d748f4f437a
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-04-22 13:28:58 +0300

    Solution

--- katas/test/test_true_counter.py
+++ katas/test/test_true_counter.py
@@ -6,3 +6,15 @@ class TestTrueCounter(unittest.TestCase):
 
         self.assertEqual(count_true_values([True,True,False,False,False,True,True]), 4)
 
+
+    def test_empty_array(self):
+
+        self.assertEqual(count_true_values([]), 0)
+
+    def test_full_true_counter(self):
+
+        self.assertEqual(count_true_values([True,True,True]), 3)
+
+    def test_zero_true_counter(self):
+
+        self.assertEqual(count_true_values([False,False]), 0)
