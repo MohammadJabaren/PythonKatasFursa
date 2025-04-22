65e032d879fbc66b3f306a9fe15a77ee5de3ce8e
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-04-22 12:52:58 +0300

    Solution

--- katas/true_counter.py
+++ katas/true_counter.py
@@ -8,6 +8,7 @@ def count_true_values(array):
 
 
 if __name__ == '__main__':
+
     sample_array = [True, False, True, True, False]
     true_count = count_true_values(sample_array)
     print(true_count)  # 3 should be printed
