2fbfd6257cd166343327516d791baa7e1cadacc9
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-04-23 20:25:20 +0300

    Solution unique string with case-insensitive

--- katas/is_unique_str.py
+++ katas/is_unique_str.py
@@ -8,6 +8,13 @@ def is_unique(string):
     Returns:
         True if all characters are unique, False otherwise
     """
+    counter_hash = {}
+    string = string.lower()
+    for element in string:
+        counter_hash[element] = counter_hash.get(element,0) + 1
+        if counter_hash[element] > 1:
+            return False
+
     return True
 
 
@@ -20,4 +27,4 @@ if __name__ == '__main__':
     print(f'"{test1}" has all unique characters: {is_unique(test1)}')  # Should be False (has repeated 'l')
     print(f'"{test2}" has all unique characters: {is_unique(test2)}')  # Should be True
     print(f'"{test3}" has all unique characters: {is_unique(test3)}')  # Should be True
-    print(f'"{test4}" has all unique characters: {is_unique(test4)}')  # Should be True
+    print(f'"{test4}" has all unique characters: {is_unique(test4)}')  # Should be False
