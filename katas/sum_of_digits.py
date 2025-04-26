4f4e7af13cd4c12c88ad9240cafe86674d5c9930
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-04-26 16:10:17 +0300

    Implement sum_of_digits function: calculate sum of all digits in a string, ignore non-digit characters

--- katas/sum_of_digits.py
+++ katas/sum_of_digits.py
@@ -19,10 +19,10 @@ def sum_of_digits(input_str):
 
 
 if __name__ == '__main__':
-    input1 = "abc1230"
-    input2 = "5 cats and 2 dogs"
-    input3 = "No digits here!"
+    INPUT1 = "abc1230"
+    INPUT2 = "5 cats and 2 dogs"
+    INPUT3 = "No digits here!"
 
-    print(f"Sum of digits in '{input1}': {sum_of_digits(input1)}")  # Should be 6
-    print(f"Sum of digits in '{input2}': {sum_of_digits(input2)}")  # Should be 7
-    print(f"Sum of digits in '{input3}': {sum_of_digits(input3)}")  # Should be 0
+    print(f"Sum of digits in '{INPUT1}': {sum_of_digits(INPUT1)}")  # Should be 6
+    print(f"Sum of digits in '{INPUT2}': {sum_of_digits(INPUT2)}")  # Should be 7
+    print(f"Sum of digits in '{INPUT3}': {sum_of_digits(INPUT3)}")  # Should be 0
