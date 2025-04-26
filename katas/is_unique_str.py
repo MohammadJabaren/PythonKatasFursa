8a2985dbcb43675e5e4d154968caf88a778b18a0
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-04-26 16:01:23 +0300

    Solution unique string with case-insensitive

--- katas/is_unique_str.py
+++ katas/is_unique_str.py
@@ -19,12 +19,13 @@ def is_unique(string):
 
 
 if __name__ == '__main__':
-    test1 = "Hello"
-    test2 = "World"
-    test3 = "Python"
-    test4 = "Unique"
+    TEST1 = "Hello"
+    TEST2 = "World"
+    TEST3 = "Python"
+    TEST4 = "Unique"
 
-    print(f'"{test1}" has all unique characters: {is_unique(test1)}')  # Should be False (has repeated 'l')
-    print(f'"{test2}" has all unique characters: {is_unique(test2)}')  # Should be True
-    print(f'"{test3}" has all unique characters: {is_unique(test3)}')  # Should be True
-    print(f'"{test4}" has all unique characters: {is_unique(test4)}')  # Should be False
+    print(f'"{TEST1}" has all unique characters: {is_unique(TEST1)}'
+          )# Should be False (has repeated 'l')
+    print(f'"{TEST2}" has all unique characters: {is_unique(TEST2)}')  # Should be True
+    print(f'"{TEST3}" has all unique characters: {is_unique(TEST3)}')  # Should be True
+    print(f'"{TEST4}" has all unique characters: {is_unique(TEST4)}')  # Should be False
