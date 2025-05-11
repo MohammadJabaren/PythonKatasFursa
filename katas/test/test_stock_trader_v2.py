368446406b4cd9e1e73ac6133d00bd5fd48dcf94
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-05-11 22:16:32 +0300

    Add unit tests for max_profit in stock trader v2
    - Test cases added for typical cases, no profit, increasing prices, single price, empty list, same prices, and profit at the end.

---
+++ katas/test/test_stock_trader_v2.py
@@ -0,0 +1,24 @@
+import unittest
+from katas.stock_trader_v2 import max_profit
+
+class TestStockTrader2(unittest.TestCase):
+    def test_typical_case(self):
+        self.assertEqual(max_profit([7, 1, 5, 3, 6, 4]), 7)
+
+    def test_no_profit(self):
+        self.assertEqual(max_profit([7, 6, 4, 3, 1]), 0)
+
+    def test_increasing_prices(self):
+        self.assertEqual(max_profit([1, 2, 3, 4, 5]), 4)
+
+    def test_single_price(self):
+        self.assertEqual(max_profit([10]), 0)
+
+    def test_empty_list(self):
+        self.assertEqual(max_profit([]), 0)
+
+    def test_same_prices(self):
+        self.assertEqual(max_profit([5, 5, 5, 5]), 0)
+
+    def test_profit_at_end(self):
+        self.assertEqual(max_profit([9, 8, 7, 1, 10]), 9)
